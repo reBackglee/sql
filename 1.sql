2100ad3e201205653a741f2986c91502b98987d9
Mert Semih Atasever <atasevermertsemih@gmail.com>
2025-07-27 19:04:58 +0300

    Add files via upload

---
+++ 1.sql
@@ -0,0 +1,9 @@
+SELECT title , description FROM film;
+
+SELECT * FROM film WHERE length BETWEEN 60 AND 75;
+
+SELECT * FROM film WHERE rental_rate = 0.99 AND replacement_cost IN (12.99 , 28.99);
+
+SELECT last_name FROM customer WHERE first_name = 'Mary';
+
+SELECT * FROM film WHERE length <= 50 AND rental_rate NOT IN (2.99 , 4.99);
